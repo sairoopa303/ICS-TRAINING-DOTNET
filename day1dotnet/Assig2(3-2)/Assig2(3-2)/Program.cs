4cba83a6261b846e615f032628795c284d5ce54a
Ragulapadu Sairoopa <ragulapadus@infinite.com>
2022-10-02 09:12:19 +0530

    first commit

---
+++ day1dotnet/Assig2(3-2)/Assig2(3-2)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class programassig2
+{
+    public static void Main(string[] args)
+    {
+        string str = "", reverse = "";
+        int Length = 0;
+
+        Console.WriteLine("Enter a word: ");
+        str = Console.ReadLine();
+        Length = str.Length - 1;
+        while (Length >= 0)
+        {
+            reverse = reverse + str[Length];
+            Length--;
+        }
+        Console.WriteLine("reverse of word is {0}", reverse);
+        Console.ReadLine();
+    }
+}
---
+++ day1dotnet/program5/program5/Program.cs
@@ -0,0 +1,17 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class program5
+{
+    public static void Main(string[] args)
+    {
+        double fahrenheit;
+        double celsius = 6;
+        Console.WriteLine("Celsius: " + celsius);
+        fahrenheit = (celsius * 9) / 5 + 32;
+        Console.WriteLine("Fahrenheit: " + fahrenheit);
+        Console.ReadLine();
+    }
+}
