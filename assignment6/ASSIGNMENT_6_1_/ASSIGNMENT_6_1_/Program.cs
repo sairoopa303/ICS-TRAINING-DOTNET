0870f4b8c39e41c41f328b5c9ea290d63c7fb24b
Ragulapadu Sairoopa <ragulapadus@infinite.com>
2022-10-16 20:13:10 +0530

    first commit

---
+++ assignment6/ASSIGNMENT_6_1_/ASSIGNMENT_6_1_/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASSIGNMENT_6_1_
+{
+class BOOKS
+{
+    static void Main(string[] args)
+    {
+        book  
+        book = title book("MUKTHI FREEDOM");
+        Console.WriteLine(book.title());
+        book = Author("ATMAN RAVI");
+        Console.WriteLine(author.Describe());
+        Console.ReadLine();
+    }
+}
+    class books
+    {
+        private string title;
+        public book(string title)
+        {
+            this.title = title;
+        }
+        public string Describe()
+        {
+            return "The name of this book is " + title;
+        }
+        public string Color
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+    }
+
